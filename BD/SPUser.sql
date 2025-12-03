8f598c071ae71a1e20e31dabac29b8db6b8b79ef
SargentoAldea <julianchirino05@gmail.com>
2025-12-02 21:33:47 -0300

    sp pal login

--- BD/SPUser.sql
+++ BD/SPUser.sql
@@ -1,1 +1,14 @@
 --aqui ponlo
+CREATE PROCEDURE sp_ValidarLogin
+    @EmailInput varchar(300)
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+
+
+    SELECT idUser, email, psswd, rol 
+    FROM Users 
+    WHERE email = @EmailInput;
+END
+GO
